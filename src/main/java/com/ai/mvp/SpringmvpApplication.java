445beb6b03755c26c80a4fcb3c5cbd7d32cd57e8
jiyongyang <1143715138@qq.com>
2026-01-29 14:11:22 +0800

    初始化项目

---
+++ src/main/java/com/ai/mvp/SpringmvpApplication.java
@@ -0,0 +1,13 @@
+package com.ai.mvp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmvpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringmvpApplication.class, args);
+	}
+
+}
